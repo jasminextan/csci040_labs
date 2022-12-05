20b8f96f25f6201e3008f20887b38573acbe1714
jasminextan <112445202+jasminextan@users.noreply.github.com>
2022-12-04 23:38:41 -0800

    Update python_praw_madlibs_lab.py

--- python_praw_madlibs_lab.py
+++ python_praw_madlibs_lab.py
@@ -18,25 +18,33 @@ SUBMISSION:
 Upload your bot's name and your `madlib.py` file to sakai.
 '''
 
+import random
+import praw
+import datetime
+import time
+reddit = praw.Reddit('bigjbot1')
+url = "https://old.reddit.com/r/cs40_2022fall/comments/yv4s9o/practice_posting_messages_here/"
+submission = reddit.submission(url=url)
+
 madlibs = [
-    "[BEEF] is a [GREAT] protien to make [SOUP] with. I love [EATING] well [COOKED] [MEALS], such as [BAKED BEANS]."
-    "I have been trying to learn how to eat healthier, such as incorporating more [BEEF] into my diet. Sometimes I think about [EATING] [BAKED BEANS], but then I ask myself is this a healthy [MEAL]? Who knows, I'll just stay and enjoy my [BEEF]."
-    "Learning to cook is hard; the last time I [COOKED] [BEEF], I ended up with a [MEAL] that looked more like [BAKED BEANS]. Ugh! I just want to make a good [SOUP]."
-    "I love the way that Bardot [COOKED] my [BAKED BEANS]. They also make a [GREAT] [BEEF] [SOUP]."
-    "If I had a choice, I would choose one [MEAL] of [LAMB CHOPS] over ten [MEALS] of [BAKED BEANS]. I believe in quality, [GREAT] food over quantity, every time."
-    "Do you prefer [BAKED BEANS] or [LAMB CHOPS]? I'm deciding what [MEAL] to cook for our potluck; I loved the [BEEF] [SOUP] that Steven [COOKED] last time."
+    "I think [BEEF] is a [GREAT] protien to make [SOUP] with. I love [EATING] well [COOKED] [MEALS], such as [BAKED BEANS]. ",
+    "I have been trying to learn how to eat healthier, such as incorporating more [BEEF] into my diet. Sometimes I think about [EATING] [BAKED BEANS], but then I ask myself is this a healthy [MEAL]? Who knows, I'll just stay and enjoy my [BEEF]. ",
+    "Learning to cook is hard; the last time I [COOKED] [BEEF], I ended up with a [MEAL] that looked more like [BAKED BEANS]. Ugh! I just want to make a good [SOUP]. ",
+    "I love the way that Big Al's [COOKED] my [BAKED BEANS]. They also make a [GREAT] [BEEF] [SOUP]. ",
+    "If I had a choice, I would choose one [MEAL] of [LAMB CHOPS] over ten [MEALS] of [BAKED BEANS]. I believe in quality, [GREAT] food over quantity, every time. ",
+    "Do you prefer [BAKED BEANS] or [LAMB CHOPS]? I'm deciding what [MEAL] to cook for our potluck; I loved the [BEEF] [SOUP] that Steven [COOKED] last time. "
     ]
 
 replacements = {
-    'BEEF' : ['LAMB', 'MUTTON', 'PORK', 'BACON', 'STEAK','CHICKEN','CHICKPEAS'],
-    'GREAT' : ['WONDERFUL','FANTASTIC','DOPE','DELICIOUS','TASTY','KILLER','GOOD'],
-    'SOUP' : ['STEW','APPETIZER','SAUCE','CHOWDER','BROTH','BISQUE','GOULASH','BOUILLON'],
-    'EATING' : ['CHOWING','CONSUMING','DEVOURING','INGESTING','MUNCHING'],
-    'COOKED' : ['PREPARED','MADE','PUT TOGETHER','ASSEMBLED','FIXED'],
-    'MEALS' : ['SNACKS','BITES','NIBBLES','DISHES'],
-    'MEAL' : ['SNACK','BITE','NIBBLE','DISH'],
-    'LAMB CHOPS' : ['CURED HAM', 'EGGS BENEDICT', 'GOAT CHEESE SKEWERS','CURLY FRIES'],
-    'BAKED BEANS' : ['GRILLED CHEESE','HAMBURGER','PEPPERONI PIZZA','SWEET POTATO FRIES','HAM AND CHEESE PANINI'],
+    'BEEF' : ['beef','lamb', 'mutton', 'pork', 'bacon', 'steak','chicken','chickpeas'],
+    'GREAT' : ['great','wonderful','fantastic','dope','delicious','tasty','killer','good'],
+    'SOUP' : ['soup','stew','appetizer','sauce','chowder','broth','bisque','goulash','boullion'],
+    'EATING' : ['eating','chowing','consuming','devouring','ingesting','munching'],
+    'COOKED' : ['cooked','prepared','made','assembled','fixed'],
+    'MEALS' : ['meals','snacks','nibbles','dishes'],
+    'MEAL' : ['meal','snack','nibble','dish'],
+    'LAMB CHOPS' : ['lamb chops','cured ham', 'eggs benedict', 'goat cheese skewers','curly fries'],
+    'BAKED BEANS' : ['baked beans','grilled cheese','pepperoni pizza','sweet potato fries'],
     }
 
 
@@ -56,3 +64,27 @@ def generate_comment():
     You do not have to worry about making the output grammatically correct inside this function.
     Instead, you should ensure that the madlibs that you create will all be grammatically correct when this substitution procedure is followed.
     '''
+
+    word = random.choice(madlibs)
+    for replacement in replacements.keys():
+        word = word.replace('[' + replacement + ']', random.choice(replacements[replacement]))
+    return word
+
+for i in range(2):
+    print(datetime.datetime.now(), ': commented, i=',i)
+    try:
+        submission.reply(generate_comment())
+    except praw.exceptions.APIException:
+        print('sleep time')
+        time.sleep(5)
+
+submission.comments.replace_more(limit=None)
+for top_level_comment in submission.comments:
+    print(top_level_comment.body)
+
+for comment in submission.comments.list():
+    try:
+        comment.reply(generate_comment())
+    except praw.exceptions.APIException:
+        print('sleep time')
+        time.sleep(5)
